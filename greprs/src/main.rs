a641d9e8480d8096d39dc20b5df4657a34c49f01
josh reuben <joshr@apester.com>
2017-07-07 11:05:12 +0300

    guessing game

---
+++ greprs/src/main.rs
@@ -0,0 +1,33 @@
+extern crate greprs;
+
+use std::env;
+use std::process;
+use std::io::prelude::*;
+
+use greprs::Config;
+
+fn main() {
+    let mut stderr = std::io::stderr();
+
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        writeln!(
+            &mut stderr,
+            "Problem parsing arguments: {}",
+            err
+        ).expect("Could not write to stderr");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = greprs::run(config) {
+        writeln!(
+            &mut stderr,
+            "Application error: {}",
+            e
+        ).expect("Could not write to stderr");
+        process::exit(1);
+    }
+}
